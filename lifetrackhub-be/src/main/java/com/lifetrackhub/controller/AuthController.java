fb4b0d4f64735e613baa0cee39464c1e2c0ff2a1
NYN31 <noyoncse310197@gmail.com>
2024-08-12 17:13:21 +0600

    fix: add log and update mapping url in controller

--- lifetrackhub-be/src/main/java/com/lifetrackhub/controller/AuthController.java
+++ lifetrackhub-be/src/main/java/com/lifetrackhub/controller/AuthController.java
@@ -6,12 +6,15 @@ import com.lifetrackhub.dto.request.RegistrationRequestDto;
 import com.lifetrackhub.dto.response.LoginResponseDto;
 import com.lifetrackhub.service.AuthService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
     private final AuthService authService;
 
@@ -21,11 +24,13 @@ public class AuthController {
 
     @PostMapping("/auth/login")
     public LoginResponseDto login(@RequestBody @Valid LoginRequestDto dto) {
+        log.info("Request enter into login controller");
         return authService.login(dto);
     }
 
     @PostMapping("/auth/registration")
     public UserDto registration(@RequestBody @Valid RegistrationRequestDto dto) {
+        log.info("Request enter into registration controller");
         return authService.registration(dto);
     }
 }
--- lifetrackhub-be/src/main/java/com/lifetrackhub/controller/UserController.java
+++ lifetrackhub-be/src/main/java/com/lifetrackhub/controller/UserController.java
@@ -4,29 +4,35 @@ import com.lifetrackhub.dto.UserDto;
 import com.lifetrackhub.entity.User;
 import com.lifetrackhub.service.UserService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController extends BaseController {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping("/test")
+    @GetMapping("/user/test")
     public String test() {
         return "Hello World!";
     }
 
     @GetMapping("/user/{email}")
     public UserDto findByEmail(@PathVariable String email) {
+        log.info("Request enter into user find by email controller");
         User user = userService.findUserByEmail(email);
         return UserDto.formEntity(user);
     }
 
-    @PutMapping("/user")
+    @PutMapping("/user/update")
     public UserDto update(@RequestBody @Valid UserDto dto) {
+        log.info("Request enter into update user controller");
         User user = userService.update(dto);
         return UserDto.formEntity(user);
     }
