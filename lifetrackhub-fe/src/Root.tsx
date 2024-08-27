577a211a01e271d5fdb11d8b68fd42394bc1e5b9
NYN31 <noyoncse310197@gmail.com>
2024-08-27 15:10:30 +0600

    wrap App component

--- lifetrackhub-fe/src/Root.tsx
+++ lifetrackhub-fe/src/Root.tsx
@@ -1,7 +1,19 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { store } from './app/store';
 
 const Root = () => {
-  return <App />;
+  return (
+    <BrowserRouter>
+      <ChakraProvider>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </ChakraProvider>
+    </BrowserRouter>
+  );
 };
 
 export default Root;
