351bbb55f985c8b1af0f314deaed00a27748f410
NYN31 <noyoncse310197@gmail.com>
2024-08-28 11:08:09 +0600

    add few random pages

---
+++ lifetrackhub-fe/src/constants/sidebar/items-title-and-path.ts
@@ -0,0 +1,18 @@
+// COMMON
+export const HOME = 'Home';
+export const HOME_PATH = '/home';
+
+export const ABOUT = 'About';
+export const ABOUT_PATH = '/about';
+
+export const SETTING = 'Setting';
+export const SETTING_PATH = '/setting';
+
+// EMPLOYEE
+export const EMPLOYEE = 'Employee';
+
+export const EMPLOYEE_LIST = 'Employee List';
+export const EMPLOYEE_LIST_PATH = '/employee';
+
+export const EMPLOYEE_DETIALS = 'Employee Detail';
+export const EMPLOYEE_DETIALS_PATH = '/employee/detail';
