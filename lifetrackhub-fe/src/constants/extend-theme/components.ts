8b48b77c2e5f1cff8d0b43e190425c54835a850b
NYN31 <noyoncse310197@gmail.com>
2024-09-13 14:56:20 +0600

    create-todo: add customise Checkbox component

---
+++ lifetrackhub-fe/src/constants/extend-theme/components.ts
@@ -0,0 +1,16 @@
+export const Components = {
+  Checkbox: {
+    baseStyle: {
+      control: {
+        _checked: {
+          bg: 'icon',
+          borderColor: 'icon',
+          _hover: {
+            bg: 'icon',
+            borderColor: 'icon',
+          },
+        },
+      },
+    },
+  },
+};
