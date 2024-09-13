dd05ce106fb38bd1a7ab2190cb4a5ef2ad2fd9e8
NYN31 <noyoncse310197@gmail.com>
2024-09-13 14:57:40 +0600

    create-todo: add all todo types

--- lifetrackhub-fe/src/types/todo.ts
+++ lifetrackhub-fe/src/types/todo.ts
@@ -1,14 +1,32 @@
-export interface ITodoItems {
-  text: string;
-  completed: boolean;
+export interface IBaseTodo {
+  id: number;
+  createdDate: string;
+  lastModifiedDate: string;
 }
 
-export interface ITodoListItem {
-  id: number;
+export interface ITodoResponse {
+  content: ITodoItemsResponse[];
+  hasNext: boolean;
+  hasPrevious: boolean;
+  pageNumber: number;
+  totalPages: number;
+}
+
+export interface ITodoItemsRequest {
   userId: number;
   title: string;
   done: boolean;
-  createdDate: string;
-  lastModifiedDate: string;
   todoItems: ITodoItems[];
+}
+
+export interface ITodoItemsResponse extends IBaseTodo {
+  userId: number;
+  title: string;
+  done: boolean;
+  todoItems: ITodoItems[];
+}
+
+export interface ITodoItems {
+  text: string;
+  completed: boolean;
 }
