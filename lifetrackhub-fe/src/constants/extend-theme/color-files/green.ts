6ab1ffd1d8a52e27245cfb5a9d787783076eb953
NYN31 <noyoncse310197@gmail.com>
2024-09-14 20:31:29 +0600

    color-customise: add two colors appearance
    - 1. remove default gray/white and cyan colors
    - 2. add orange and green color
    - 3. make purple as a default app color

---
+++ lifetrackhub-fe/src/constants/extend-theme/color-files/green.ts
@@ -0,0 +1,39 @@
+export const greenColor = {
+  danger: '#F56565',
+  primary: '#F2F2F2',
+  body: '#FFFFFF',
+  icon: '#38A169', // 500
+  link: 'blue',
+  alert: '#FED7D7',
+  btn: {
+    bg: '#38A169', // 500
+    text: '#fff',
+    bg_disable: 'gray',
+    text_disable: '#cccccc',
+  },
+  avatar: {
+    bg: '#38A169', // 500
+    text: '#FFFFFF',
+  },
+  gray: {
+    default: '#CBD5E0',
+    hover: 'gray',
+  },
+  sidebar: {
+    bg: '#ffffff',
+    text: '#000000',
+    hover_bg: '#C6F6D5', // 100
+    hover_text: '#000',
+  },
+  navbar: {
+    bg: '#F0FFF4', // 50
+    text: '#000',
+  },
+  menu: {
+    bg: '#CBD5E0',
+    list_bg: '#FFF',
+    list_border: '#CBD5E0',
+    list_text: '#171923',
+    list_hover: '#C6F6D5', // 100
+  },
+};
