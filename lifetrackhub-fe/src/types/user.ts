b8dd14852aec71fa000ad78fc829030a7eb0554b
NYN31 <noyoncse310197@gmail.com>
2024-09-15 21:46:10 +0600

    profile: update user types

--- lifetrackhub-fe/src/types/user.ts
+++ lifetrackhub-fe/src/types/user.ts
@@ -6,47 +6,47 @@ export interface IUser extends IBase {
   email: string;
   enabled: boolean;
   role: string;
-  userDetails: IUserDetails;
+  userDetails?: IUserDetails | null;
 }
 
 export interface IUserDetails {
-  objective: string;
-  skills: ISkills[];
-  profileImage: any;
-  cv: any;
-  experiences: IExperience;
-  educations: IEducation;
-  achivemenets: IAchievement;
-  socialLinks: ISocialLink;
+  objective?: string;
+  skills?: ISkills[];
+  profileImage?: any;
+  cv?: any;
+  experiences?: IExperience[];
+  educations?: IEducation[];
+  achivemenets?: IAchievement[];
+  socialLinks?: ISocialLink[];
 }
 
 export interface ISkills {
-  name: string;
-  description: string;
+  name?: string;
+  description?: string;
 }
 
 export interface IExperience {
-  name: string;
-  yearOfExperience: number;
-  description: string;
+  name?: string;
+  yearOfExperience?: number;
+  description?: string;
   link?: string;
 }
 
 export interface IEducation {
-  title: string;
-  institution: string;
-  graduationStartDate: string;
-  graduationEndDate: string;
-  result: string;
+  title?: string;
+  institution?: string;
+  graduationStartDate?: string;
+  graduationEndDate?: string;
+  result?: string;
 }
 
 export interface IAchievement {
-  title: string;
-  description: string;
+  title?: string;
+  description?: string;
   link?: string;
 }
 
 export interface ISocialLink {
-  name: string;
+  name?: string;
   link?: string;
 }
