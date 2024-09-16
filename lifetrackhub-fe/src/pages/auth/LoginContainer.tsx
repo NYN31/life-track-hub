79ed2a3c5c8d204ee4046aa162be15b57514e9f7
NYN31 <noyoncse310197@gmail.com>
2024-09-16 08:43:04 +0600

    profile: update home path

--- lifetrackhub-fe/src/pages/auth/LoginContainer.tsx
+++ lifetrackhub-fe/src/pages/auth/LoginContainer.tsx
@@ -1,10 +1,11 @@
 import { Navigate } from 'react-router-dom';
 import Login from '../../components/auth/Login';
-import { HOME_PATH } from '../../constants/sidebar/items-title-and-path';
+import { PROFILE_DETAILS_PATH } from '../../constants/sidebar/items-title-and-path';
 
 const LoginContainer = () => {
   const isPresenceAccessToken = localStorage.getItem('accessToken');
-  if (isPresenceAccessToken) return <Navigate to={HOME_PATH} replace={true} />;
+  if (isPresenceAccessToken)
+    return <Navigate to={PROFILE_DETAILS_PATH} replace={true} />;
 
   return <Login />;
 };
