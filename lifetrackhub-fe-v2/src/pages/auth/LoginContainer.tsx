6f6d949926946eb4ef5c15032f337e84a341d0f2
NYN31 <noyoncse310197@gmail.com>
2025-07-08 12:51:08 +0600

    get blog details page bug fix

--- lifetrackhub-fe-v2/src/pages/auth/LoginContainer.tsx
+++ lifetrackhub-fe-v2/src/pages/auth/LoginContainer.tsx
@@ -1,6 +1,18 @@
+import { useNavigate } from 'react-router-dom';
 import LoginForm from '../../components/auth/LoginForm';
+import { useEffect } from 'react';
+import Spinner from '../../components/common/Spinner';
 
 const LoginContainer = () => {
+  const navigate = useNavigate();
+
+  const accessToken = localStorage.getItem('accessToken');
+
+  useEffect(() => {
+    if (accessToken) navigate(-1);
+  }, [navigate]);
+
+  if (accessToken) return <Spinner />;
   return <LoginForm />;
 };
 
