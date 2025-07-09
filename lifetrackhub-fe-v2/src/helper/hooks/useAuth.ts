e825ceeaf90de731a78405b7ecc7bd15c249c5e8
NYN31 <noyoncse310197@gmail.com>
2025-07-09 11:42:44 +0600

    navigation related bug fix

--- lifetrackhub-fe-v2/src/helper/hooks/useAuth.ts
+++ lifetrackhub-fe-v2/src/helper/hooks/useAuth.ts
@@ -1,13 +1,19 @@
+import { useDispatch } from 'react-redux';
 import { logoutClearingLocalStorage } from '../local-storage/clear-local-storage';
 import { getAllLocalStoreValue } from '../local-storage/get-local-store-values';
+import { userLoggedIn, userLoggedOut } from '../../features/auth/authSlice';
 
 export default function useAuth() {
+  const dispatch = useDispatch();
+
   const { name, accessToken, email, role } = getAllLocalStoreValue();
 
   if (name && accessToken && email && role) {
+    dispatch(userLoggedIn({ name, accessToken, email, role }));
     return true;
   } else {
     logoutClearingLocalStorage();
+    dispatch(userLoggedOut());
     return false;
   }
 }
