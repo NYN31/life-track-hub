1403ef6835de262eabe13c36b983d51f08757489
NYN31 <noyoncse310197@gmail.com>
2025-08-19 16:42:42 +0600

    todo card design updated

--- lifetrackhub-fe-v2/src/components/todo/TodoList.tsx
+++ lifetrackhub-fe-v2/src/components/todo/TodoList.tsx
@@ -15,49 +15,55 @@ const TodoList: React.FC<TodoListProps> = ({ todos, onEdit }) => {
   }
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
+    <div className="grid grid-cols-1 gap-4 mt-8">
       {todos.map(todo => (
         <div
           key={todo.id}
-          className="common-box flex flex-col p-2 md:p3 lg:p-4"
+          className="common-box flex flex-row justify-between items-center"
         >
-          <div className="flex items-center gap-3 mb-4">
-            <div>
-              <div
-                className={`w-3 h-3 rounded-full ${
-                  todo.todoStatus === 'DONE' ? 'bg-green-400' : 'bg-yellow-400'
-                } border-2 border-white shadow`}
-              ></div>
+          <div>
+            <div className="flex items-center gap-3 mb-4 mr-4">
+              <div>
+                <div
+                  className={`w-3 h-3 rounded-full ${
+                    todo.todoStatus === 'DONE'
+                      ? 'bg-green-400'
+                      : 'bg-yellow-400'
+                  } border-2 border-white shadow`}
+                ></div>
+              </div>
+              <h3 className="line-clamp-2">{todo.title}</h3>
             </div>
-            <h3 className="line-clamp-2">{todo.title}</h3>
+            <div className="text-center ">
+              <div className="flex flex-wrap md:flex-row gap-2">
+                <span
+                  className={`px-3 py-1 rounded-full text-xs font-bold tracking-wide info-box ${
+                    todo.todoStatus === 'DONE'
+                      ? 'bg-green-100 text-green-700'
+                      : 'bg-yellow-100 text-yellow-700'
+                  }`}
+                >
+                  {todo.todoStatus}
+                </span>
+                <span className="info-box">
+                  Created:{' '}
+                  {todo.createdDate
+                    ? new Date(todo.createdDate).toLocaleString()
+                    : '-'}
+                </span>
+                <span className="info-box">
+                  Last Modified:{' '}
+                  {todo.lastModifiedDate
+                    ? new Date(todo.lastModifiedDate).toLocaleString()
+                    : '-'}
+                </span>
+              </div>
+            </div>
           </div>
-          <div className="flex flex-wrap gap-2 mb-4">
-            <span
-              className={`px-3 py-1 rounded-full text-xs font-bold tracking-wide ${
-                todo.todoStatus === 'DONE'
-                  ? 'bg-green-100 text-green-700'
-                  : 'bg-yellow-100 text-yellow-700'
-              }`}
-            >
-              {todo.todoStatus}
-            </span>
-            <span className="info-box">
-              Created:{' '}
-              {todo.createdDate
-                ? new Date(todo.createdDate).toLocaleString()
-                : '-'}
-            </span>
-            <span className="info-box">
-              Last Modified:{' '}
-              {todo.lastModifiedDate
-                ? new Date(todo.lastModifiedDate).toLocaleString()
-                : '-'}
-            </span>
-          </div>
-          <div className="flex justify-end mt-auto">
+          <div>
             <OnClickButton
               action={() => onEdit(todo)}
-              text={'Edit'}
+              text="Edit"
               icon={<FaRegEdit size={18} />}
             />
           </div>
