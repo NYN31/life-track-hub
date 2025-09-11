b95800e9bb69e4d2b0fd06b5b3df777ef7167c61
NYN31 <noyoncse310197@gmail.com>
2025-09-11 16:24:19 +0600

    fix issue in blog comment user profile picture issue

--- lifetrackhub-be/src/main/java/com/lifetrackhub/dto/response/BlogCommentResponseDto.java
+++ lifetrackhub-be/src/main/java/com/lifetrackhub/dto/response/BlogCommentResponseDto.java
@@ -1,11 +1,15 @@
 package com.lifetrackhub.dto.response;
 
+import com.lifetrackhub.dto.blob.UserDetails;
 import com.lifetrackhub.entity.BlogComment;
 import com.lifetrackhub.entity.User;
 import lombok.Data;
 import lombok.ToString;
 
 import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 @Data
 @ToString
@@ -23,14 +27,25 @@ public class BlogCommentResponseDto {
     public static BlogCommentResponseDto formEntity(
             BlogComment blogComment) {
         BlogCommentResponseDto dto = new BlogCommentResponseDto();
-        User user = blogComment.getUser();
+        Map<String, String> userInfo = getUserInfo(blogComment.getUser());
 
         dto.setCommentId(blogComment.getId());
-        dto.setUsername(user.getFirstname() + " " + user.getLastname());
-        dto.setUserProfilePictureUrl("");
+        dto.setUsername(userInfo.get("username"));
+        dto.setUserProfilePictureUrl(userInfo.get("userProfilePictureUrl"));
         dto.setContent(blogComment.getContent());
         dto.setCreatedDate(blogComment.getCreatedDate());
 
         return dto;
+    }
+
+    public static Map<String, String> getUserInfo(User user) {
+        Map<String, String> userInfo = new HashMap<>();
+        String profileImageUrl = Optional.ofNullable(user.getUserDetails())
+                .map(UserDetails::getProfileImagePath)
+                .orElse(null);
+
+        userInfo.put("username", user.getFirstname() + " " + user.getLastname());
+        userInfo.put("userProfilePictureUrl", profileImageUrl);
+        return userInfo;
     }
 }
